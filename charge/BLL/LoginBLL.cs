ffc11a13deb3b65f06f110b14a509cb718b537dc
MungerLi <MungerLi6688@users.noreply.github.com>
2018-04-01 15:58:08 +0800

    2018年4月1日下午下课同步

--- charge/BLL/LoginBLL.cs
+++ charge/BLL/LoginBLL.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace BLL
 {
     public class LoginBLL
     {
+        public bool UserBLL(Entity.User_Info user_Info)
+        {
+            bool flag;
+
+            Factory.LoginFactory fact = new Factory.LoginFactory();
+
+            IDAL.LoginIDAL idal = fact.CreaterUser();
+
+            DataTable table = idal.selectUser(user_Info);
+
+            if (table.Rows.Count = 0)
+            {
+                flag = false;
+            }
+            else
+            {
+                flag = true;
+            }
+            return flag;
+        }
     }
 }
